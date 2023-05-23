71180e9ac347629534db1b4ea85179e8b1b72657
Gigizix <giovanacaroline666@gmail.com>
2023-05-23 08:17:28 -0400

    commit

---
+++ script.js
@@ -0,0 +1,23 @@
+class Produto
+constructor(nome,datadeCadastro,descricao,preco){
+    this.nome= nome;
+    this.datadeCadastro= datadeCadastro;
+    this.descricao= descricao;
+    this.preco= preco;
+}
+mostrar_produto(){
+    return this.nome + this.datadeCadastro +this.descricao + this.preco,this.imagemDestaques
+}
+
+class produtosDestaques extends produtos{
+    constructor(nome,datadeCadastro,descricao,preco){
+    this.nome= nome
+    this.datadeCadastro= datadeCadastro
+    this.descricao= descricao
+    this.preco= preco
+   this.Destaques
+
+    }
+}
+const produtos = new Produto ("cachorro Golden","Fêmea","R$:500,00")https://www.petlove.com.br/images/breeds/193223/profile/original/golden_retriever-p.jpg?1532539102
+const produtosDestaques = new produtosDestaques ("mostrar_produto_destaque");
